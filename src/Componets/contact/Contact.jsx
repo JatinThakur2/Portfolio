e11611c295d75d42607a9a3f3bea047100d03071
Jatin Thakur <jatinthakur3333@gmail.com>
2021-08-26 20:38:52 +0530

    contact

--- src/Componets/contact/Contact.jsx
+++ src/Componets/contact/Contact.jsx
@@ -23,28 +23,14 @@ export default function Contact() {
             </div>
             <div className="right">
                     <h2>Contact</h2>
-                    <form onSubmit={sendEmail}>
-                     <div className="row pt-5 mx-auto">
-                        <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
-                        </div>
-                        <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send Message"></input>
-                        </div>
-                    </div>
+            <form onSubmit={sendEmail}>
+                            <input type="text" placeholder="Name" name="name"/>
+                            <input type="email" placeholder="Email Address" name="email"/>
+                            <input type="text" placeholder="Subject" name="subject"/> 
+                            <textarea placeholder="Your message" name="message"></textarea>
+                            <button type="submit" className="btn" value="">Send Message</button>                       
                 </form>
-                        
-                
-                 
+     
                 </div>
         </div>
     )
